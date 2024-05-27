24044c95cd8ccd0991a7883566ba6ba004a2ce9d
Khadija <dija5631@gmail.com>
2024-05-27 15:00:16 +0200

    run

--- server.js
+++
@@ -1,6 +0,0 @@
-const http = require ('http');
-const app = require ('./app')
-const serser = http.createServer(app);
-serser.listen(3000,()=>{
-    console.log('Server is running at localhost:3000');
-});
