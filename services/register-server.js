d958e1ee02cbe5705e94e814644cef9bfd08e409
Khadija <dija5631@gmail.com>
2024-05-29 10:07:11 +0200

    rectification

--- services/register-server.js
+++ services/register-server.js
@@ -1,5 +1,5 @@
 const http = require ('http');
-const app = require ('./app')
+const app = require ('../register')
 const serser = http.createServer(app);
 serser.listen(8000,()=>{
     console.log('Server is running at localhost:8000');
