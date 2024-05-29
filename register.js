f0ae7c33edb240ce75d57df50824d03b1b868c13
Khadija <dija5631@gmail.com>
2024-05-29 16:53:52 +0200

    test-2

---
+++ register.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app  = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const RouteUsers = require('./router/register');
+mongoose.connect('mongodb+srv://dija5631:dbrestau@cluster0.a5lixnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+.then(() => {
+    console.log('connexion success !')
+})
+.catch((error) => {
+    console.log('Erreur de connexion');
+});
+app.post('/',(req,res) => {
+    res.send('/api/users/')
+    console.log("réussi avec succès")
+});
+
+
+app.use(bodyParser.json())
+app.use('/api/users/', RouteUsers);
+
+module.exports = app;
