3024ce2b023bc1bfd9c25f69b8e3c2cdcff56404
boubacar26 <boubacar.ndiaye.me@gmail.com>
2024-05-30 15:33:47 +0000

    Correction de l'erreur CORS

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ const ProdCtrl = require('./controllers/register');
 // Middleware
 app.use(bodyParser.json());
 
+// CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+});
+
+
 
 
 GetPassword.use((req, res, next) => {
