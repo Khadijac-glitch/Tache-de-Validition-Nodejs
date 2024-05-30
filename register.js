20d0e66d60a655d20bf1321e72ea2b206440cec5
Khadija <dija5631@gmail.com>
2024-05-30 17:46:50 +0200

    commit du newsletter

--- register.js
+++ register.js
@@ -3,6 +3,8 @@ const app  = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const RouteUsers = require('./router/register');
+// const RouteEmails = require('./router/email');
+
 mongoose.connect('mongodb+srv://dija5631:dbrestau@cluster0.a5lixnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
  
 )
@@ -17,8 +19,10 @@ mongoose.connect('mongodb+srv://dija5631:dbrestau@cluster0.a5lixnb.mongodb.net/?
 app.use(bodyParser.json())
 app.use('/users/', RouteUsers);
 app.use('/admin/', RouteUsers);
+app.use('/email/', RouteUsers);
+
 
 app.listen(8000, () => {
-    console.log(`Serveur en cours d'exécution sur le port 8000`);
+    console.log(`Serveur en cours d'exécution sur le port ...`);
   });
 module.exports = app;
--- router/email.js
+++ router/email.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const sendEmail = require('../models/email');
+const sendEmail = require('../models/email'); 
 
-// Route pour envoyer des e-mails
-router.post('/send', async (req, res) => {
+router.post('/send', (req, res) => {
   const { to, subject, text } = req.body;
 
-  try {
-    await sendEmail(to, subject, text);
-    res.status(200).json({ message: 'Email sent successfully' });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).json({ message: 'Failed to send email' });
-  }
+  sendEmail(to, subject, text)
+    .then(() => {
+      res.status(200).json({ message: 'Email envoyer avec success' });
+    })
+    .catch((err) => {
+      console.error(err.message);
+      res.status(500).json({ message: ' Error email' });
+    });
 });
 
 module.exports = router;
+
+
+// const express  = require('express')
+// const router = express.Router()
+// const ProdCtrl = require('../controllers/email');
+
+// // User CRUD
+// router.post('/send-email', ProdCtrl.sendEmail);
+
+
+ 
