df811730113565da81269b222f713eecceaa1237
Khadija <dija5631@gmail.com>
2024-06-03 13:31:05 +0200

    recuperation branch connexion-Back

--- services/register-server.js
+++ services/register-server.js
@@ -1,12 +1,7 @@
 const http = require ('http');
 const app = require ('../register');
-// const email= require ('../register');
 const { register } = require('module');
 const app = http.createServer(register);
 app.listen(8000,()=>{
     console.log('Server is running at localhost:...');
 });
-// const email = http.createServer(register);
-// email.listen(8000,()=>{
-//     console.log('Server is running at localhost:..');
-// });
