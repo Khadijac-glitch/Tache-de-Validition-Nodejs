69b6e012caa9d93aecce4622323c14c349d649ce
Khadija <dija5631@gmail.com>
2024-06-10 14:56:57 +0200

    correction1

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ const predictRoutes = require('./router/predict');
 
 
 mongoose.connect('mongodb+srv://dija5631:dbrestau@cluster0.a5lixnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',)
-// mongoose.connect('mongodb+srv://elzofils:sadia2020@cluster0.m6lkoeh.mongodb.net/nodeapis?retryWrites=true&w=majority&appName=Cluster0',
-// )
-
-// mongoose.connect('mongodb+srv://madjiguened835:Hr0NWZprD4lcz1BH@get-password.sot5wfh.mongodb.net/?retryWrites=true&w=majority&appName=get-password',)
 
 .then(() => {
     console.log('connexion success !')
@@ -31,7 +27,6 @@ app.use(bodyParser.json())
 
 app.use('/api/register', RouteUsers);
 app.use('/api/auth', require('./router/auth'));
-// app.use('/api/forguotPassword', require('./routes/forguotPassword'));
 app.use('/api/admin/', RouteUsers);
 app.use('/api/email', emailRoutes); 
 app.use('/api/passwords', userRoutes);
