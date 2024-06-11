c7af200cf285122b5c0989a912912e867c79da0d
boubacar26 <boubacar.ndiaye.me@gmail.com>
2024-06-11 17:30:29 +0000

    Fusion avec la branche a jour

--- models/register.js
+++ models/register.js
@@ -1,44 +1,4 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-// const validator = require("validator");
-=======
 const mongoose = require('mongoose');
-const validator = require('validator');
-const userRegister = mongoose.Schema({
-    firstName:{
-    type:String,
-    required:true,
-    },
-    lastName:{
-        type:String,
-        required:true,
-        },
-    email: {
-        type:String,
-        required:true,
-        validate(v) {
-        if(!validator.isEmail(v)) throw new Error('Email non valide!');   
-    }
-    },
-    number:{
-        type:Number,
-        required:true,
-        },
-    password: {
-        type:String,
-        required:true,
-        validate(v) {
-        if(!validator.isLength(v,{ min:4,max:20 })) throw new Error('Le mot de passe doit etre entre 4 et 20 caractères!');
-        }
-    },
-    confirmPassword: {
-        type:String,
-        required:true,
-        validate(v) {
-        if(!validator.isLength(v,{ min:4,max:20 })) throw new Error('Le mot de passe doit etre entre 4 et 20 caractères!');
-        }
-    }
->>>>>>> 5d8c406dcf031b7e6bce2ef0694a844234b9045c
 
 const user = mongoose.Schema({
   firstName: {
@@ -76,12 +36,4 @@ const user = mongoose.Schema({
     default: 'user'
 },
 });
-<<<<<<< HEAD
- 
-
-
-
-module.exports = mongoose.model("Register", user);
-=======
-module.exports = mongoose.model('Register', userRegister);
->>>>>>> 5d8c406dcf031b7e6bce2ef0694a844234b9045c
+module.exports = mongoose.model('Register', user);
