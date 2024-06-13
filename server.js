f327d0d017b6cd9102fdaa4bfce4a7ae4fd4ba55
Khadija <dija5631@gmail.com>
2024-06-13 08:48:18 +0200

    deploy-succes

--- server.js
+++ server.js
@@ -14,14 +14,7 @@ const emailRoutes = require('./router/email');
 const adminRouteProduit = require("./router/liste-produit");
 const localisationRoutes = require("./router/localisation-resto");
 
-//Connexion base de donnees
-// mongoose.connect('mongodb+srv://boubacarndiaye:boubacar@route-liste-produit.9hnsns9.mongodb.net/?retryWrites=true&w=majority&appName=Route-liste-produit')
-
 mongoose.connect('mongodb+srv://dija5631:dbrestau@cluster0.a5lixnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',)
-// mongoose.connect('mongodb+srv://elzofils:sadia2020@cluster0.m6lkoeh.mongodb.net/nodeapis?retryWrites=true&w=majority&appName=Cluster0',
-// )
-
-// mongoose.connect('mongodb+srv://madjiguened835:Hr0NWZprD4lcz1BH@get-password.sot5wfh.mongodb.net/?retryWrites=true&w=majority&appName=get-password',)
 
 .then(() => {
     console.log('connexion success !')
@@ -49,22 +42,9 @@ app.use("/admin", adminRouteProduit);
 app.use("/user", adminRouteProduit);
 app.use(localisationRoutes);
 
-
-// app.use('/api/passwords', userRoutes);
-// app.use('/api/reservation', reservationRoutes);
-
-
-// Redirection de la racine vers /api-docs
-// app.get('/', (req, res) => {
-//   res.redirect('/api-docs');
-// });
-
-
-
 app.listen(8080, () => {
     console.log(`Serveur en cours d'exécution sur le port 8080`);
   });
 
 exports.module = app;
 
-// exports.api= functions.https.onRequest (app);
