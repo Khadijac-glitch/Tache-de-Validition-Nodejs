b91949d8d93aabd4963e96fe4fc2db3c52c127e3
boubacar26 <boubacar.ndiaye.me@gmail.com>
2024-07-02 11:56:01 +0000

    Ajout des images

--- router/liste-produit.js
+++ router/liste-produit.js
@@ -32,15 +32,19 @@ const upload = require("../multer.js"); // Chemin vers la configuration multer
  *       500:
  *         description: Erreur du serveur
  */
-router.post("/liste-produits", async (req, res, next) => {
-  try {
-    const produit = new Produit(req.body);
-    const saveProduit = await produit.save();
-    res.status(201).send(saveProduit);
-  } catch (e) {
-    res.status(404).send(e);
-  }
-  
+router.post("/liste-produits", upload.single("image"), async (req, res, next) => {
+    try {
+      const produit = new Produit({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.file ? req.file.path : null,
+      });
+      const saveProduit = await produit.save();
+      res.status(201).send(saveProduit);
+    } catch (e) {
+      res.status(500).send(e);
+    }
   }
 );
 
@@ -61,7 +65,7 @@ router.get("/liste-produits", async (req, res, next) => {
     const produits = await Produit.find({});
     res.send(produits);
   } catch (e) {
-    res.status(404).send(e);
+    res.status(500).send(e);
   }
 });
 
@@ -92,7 +96,7 @@ router.get("/liste-produits/:id", async (req, res, next) => {
     if (!produits) return res.status(404).send("Produit non trouvé");
     res.send(produits);
   } catch (e) {
-    res.status(404).send(e);
+    res.status(500).send(e);
   }
 });
 
@@ -131,18 +135,26 @@ router.get("/liste-produits/:id", async (req, res, next) => {
  *       500:
  *         description: Erreur du serveur
  */
-router.patch("/liste-produits/:id", async (req, res, next) => {
-  const produitId = req.params.id;
-  try {
-    const produits = await Produit.findByIdAndUpdate(produitId, req.body, {
-      new: true,
-    });
-    if (!produits) return res.status(404).send("Produit non trouvé");
-    res.send(produits);
-  } catch (e) {
-    res.status(404).send(e);
+router.patch(  "/liste-produits/:id", upload.single("image"), async (req, res, next) => {
+    const produitId = req.params.id;
+    const updates = {
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+    };
+    if (req.file) {
+      updates.image = req.file.path; // Met à jour le chemin de l'image
+    }
+    try {
+      const produits = await Produit.findByIdAndUpdate(produitId, updates, {
+        new: true,
+      });
+      if (!produits) return res.status(404).send("Produit non trouvé");
+      res.send(produits);
+    } catch (e) {
+      res.status(500).send(e);
+    }
   }
-}
 );
 
 
@@ -173,7 +185,7 @@ router.delete("/liste-produits/:id", async (req, res, next) => {
     if (!produits) return res.status(404).send("Produit non trouvé");
     res.send(produits);
   } catch (e) {
-    res.status(404).send(e);
+    res.status(500).send(e);
   }
 });
 
